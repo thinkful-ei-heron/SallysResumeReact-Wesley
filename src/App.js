a964e6c67b34be16a2bed18d16cbd2dd8150acb7
Wesley Jacobs <wesleyjacobs@Wesleys-MacBook-Pro.local>
2019-10-07 11:06:21 -0700

    completed w/o stretch Goals

--- src/App.js
+++ src/App.js
@@ -5,20 +5,42 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="Sallys Resume">
+        <h1>Sally here, welcome to my Resumé</h1>
       </header>
+      <main>
+        
+        <section>
+          <h4>Contact me</h4>
+            <img src="https://ia.media-imdb.com/images/M/MV5BMTY5NzE3NzU3MF5BMl5BanBnXkFtZTgwMjg0NTQ5MDE@._V1_UX214_CR0,0,214,317_AL_.jpg" className="App-logo" alt="logo" />
+            <p>Sally Suzie Shannon</p>
+            <a href="mailto:Sally@suzie.com">Sally@SallySuzie.com</a>
+            <p>(555)555-5555</p>
+        </section>
+
+        <section>
+          <h4>Education</h4>
+          <ul>
+            <li>B.A Deconstructing meaning and tone from text / School of Life</li>
+            <li>Engineering Immersion / Thinkful</li>
+          </ul>
+        </section>
+
+        <section>
+          <h4>Experience</h4>
+          <ul>
+            <li>Bob's Burgers</li>
+            <li>Dunder Mifflin Paper Company</li>
+          </ul>
+        </section>
+      </main>
+
+
+
+
+
+       
+      
     </div>
   );
 }
